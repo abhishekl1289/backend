2779a517079b8abee93a0c7773e8226a3e3fc31c
abhishekl1289 <801305abhishekl@gmail.com>
2024-04-10 17:31:12 +0530

    stored rooms

--- models/user-model.js
+++ models/user-model.js
@@ -1,14 +1,25 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-const userSchema=new Schema(
-    {
-        phone: { type: String, required: true},
-        name: {type: String, required: false},
-        avatar: {type: String, required: false},
-        activated: { type: Boolean, required: false, default: false},
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userSchema = new Schema(
+  {
+    phone: { type: String, required: true },
+    name: { type: String, required: false },
+    avatar: {
+      type: String,
+      required: false,
+      get: (avatar) => {
+        if(avatar)
+        {
+          return `${process.env.BASE_URL}${avatar}`;
+        }
+        return avatar;
+      },
     },
-    {
-        timestamps: true,
-    }
+    activated: { type: Boolean, required: false, default: false },
+  },
+  {
+    timestamps: true,
+    toJSON: { getters: true },
+  }
 );
-module.exports=mongoose.model('User',userSchema,'users');
+module.exports = mongoose.model("User", userSchema, "users");
