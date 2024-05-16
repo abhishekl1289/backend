c1f38a12e55568f77903bc7de2f0ae8c6ba0b0de
abhishekl1289 <801305abhishekl@gmail.com>
2024-05-17 00:55:10 +0530

    allow multiple users

--- server.js
+++ server.js
@@ -37,12 +37,52 @@ io.on('connection',(socket)=>{
         socketUserMapping[socket.id]=user;
         const clients=Array.from(io.sockets.adapter.rooms.get(roomId) || []);
         clients.forEach((clientId)=>{
-            io.to(clientId).emit(ACTIONS.ADD_PEER,{});
+            io.to(clientId).emit(ACTIONS.ADD_PEER,{
+                peerId:socket.id,
+                createOffer:false,
+                user,
+            });
+            socket.emit(ACTIONS.ADD_PEER,{
+                peerId:clientId,
+                createOffer:true,
+                user:socketUserMapping[clientId],
+            });
         });
-        socket.emit(ACTIONS.ADD_PEER,{});
+        
         socket.join(roomId);
-        // console.log(clients);
+        
     });
 
+    socket.on(ACTIONS.RELAY_ICE,({peerId,icecandidate})=>{
+        io.to(peerId).emit(ACTIONS.ICE_CANDIDATE,{
+            peerId:socket.id,
+            icecandidate,
+        });
+    });
+
+    socket.on(ACTIONS.RELAY_SDP,({peerId,sessionDescription})=>{
+        io.to(peerId).emit(ACTIONS.SESSION_DESCRIPTION,{
+            peerId:socket.id,
+            sessionDescription,
+        });
+    });
+    const leaveRoom=({roomId})=>{
+        const {rooms}=socket;
+        Array.from(rooms).forEach((roomId)=>{
+            const clients=Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+            clients.forEach((clientId)=>{
+                io.to(clientId).emit(ACTIONS.REMOVE_PEER,{peerId:socket.id,
+                    userId:socketUserMapping[socket.id].id,
+                });
+                socket.emit(ACTIONS.REMOVE_PEER,{
+                    peerId:clientId,
+                    userId:socketUserMapping[clientId].id
+                })
+            });
+            socket.leave(roomId);
+        });
+        delete socketUserMapping[socket.id];
+    };
+    socket.on(ACTIONS.LEAVE,leaveRoom);
 });
 server.listen(PORT,()=>console.log(`Listening on port ${PORT}`));
